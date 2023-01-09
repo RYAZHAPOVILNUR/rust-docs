74416320efb97d67b504a183b257bd4bc206a636
ryazhapov-ilnur <ilnur_abu_yahya@mail.ru>
2023-01-09 10:28:07 +0600

    cargo init

---
+++ hello_cargo/target/debug/deps/hello_cargo-e8e8059e0e2419bd.d
@@ -0,0 +1,5 @@
+/Users/ilnur_ryazhapov/Desktop/projects/my-projects/rust-docs/hello_cargo/target/debug/deps/hello_cargo-e8e8059e0e2419bd.rmeta: src/main.rs
+
+/Users/ilnur_ryazhapov/Desktop/projects/my-projects/rust-docs/hello_cargo/target/debug/deps/hello_cargo-e8e8059e0e2419bd.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/ilnur_ryazhapov/Desktop/projects/my-projects/rust-docs/hello_cargo/target/debug/hello_cargo: /Users/ilnur_ryazhapov/Desktop/projects/my-projects/rust-docs/hello_cargo/src/main.rs
